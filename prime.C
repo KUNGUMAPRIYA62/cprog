6db0ad99e69349ab397f2683092bc34e06ad923e
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-06-03 11:38:10 +0530

    prime.C
    prime

---
+++ prime.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,num,count=0;
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+if(num%i==0)
+count++;
+}
+if(count==2)
+{
+printf("Yes,%d is a prime number",num);
+}
+else{
+printf("No,%d is not a prime number",num);
+}
+}
