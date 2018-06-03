34eebbc142ddd8be461892a166a9522906c6cefe
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-06-03 11:54:09 +0530

    oddinrange.C
    odd

--- oddinrange.C
+++ oddinrange.C
@@ -1,11 +1,18 @@
 #include <stdio.h>
-void main();
+void main()
 {
-    int lowerLimit, upperLimit;
+    int i,lowerLimit, upperLimit;
     printf("Enter lower limit: ");
     scanf("%d", &lowerLimit);
     printf("Enter upper limit: ");
     scanf("%d", &upperLimit);
     printf("Odd Numbers from %d to %d are: ", lowerLimit, upperLimit);
-    printEvenOdd(lowerLimit, upperLimit); 
+    if(lowerLimit%2==0)
+    {
+    	lowerLimit++;
+    }
+    for(i=lowerLimit;i<=upperLimit;i+=2)
+    {
+    	printf("%d\t",i);
+    }
 }
