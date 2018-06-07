33ab92d553aefbc5ede20f101fbdea113df62ef1
KUNGUMAPRIYA62 <35221138+KUNGUMAPRIYA62@users.noreply.github.com>
2018-06-07 22:01:21 +0530

    primeinrange.C
    prime in range

--- primeinrange.C
+++ primeinrange.C
@@ -1,8 +1,17 @@
-#include <stdio.h>
-void main();
+#include<stdio.h>
+void main()
 {
-    int lowerLimit, upperLimit;
-    printf("Enter the lower and upper limit to list primes: ");
-    scanf("%d%d", &lowerLimit, &upperLimit);
-    printPrimes(lowerLimit, upperLimit);
+int n,m,i,j;
+scanf("%d %d",&n,&m);
+for(i=n;i<m;i++)
+{
+int count=0;
+for(j=2;j<i;j++)
+{
+if(i%j==0)
+count++;
+}
+if(count==0)
+printf("%d\t",i);
+}
 }
